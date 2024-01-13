16f8d1aa3bfaa3a15dd42a649499750b1ef06b1e
devkgm <gm.kim333@gmail.com>
2024-01-13 21:18:02 +0900

    feat fileManager

---
+++ src/main/java/com/devkgm/app/util/FileManager.java
@@ -0,0 +1,33 @@
+package com.devkgm.app.util;
+
+import java.io.File;
+import java.util.UUID;
+
+import javax.servlet.ServletContext;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileManager {
+	@Autowired
+	private ServletContext servletContext;
+	
+	public String saveFile(String path, MultipartFile file) throws Exception {
+		path = servletContext.getRealPath(path);
+		File f = new File(path);
+		
+		if(f.isFile()) {
+			throw new Exception();
+		}
+		
+		if(!f.exists()) {
+			f.mkdirs();
+		}
+		String fileName = UUID.randomUUID().toString() + "_" + file.getOriginalFilename();
+	
+		f = new File(f, fileName);
+		
+		file.transferTo(f);
+		return fileName;
+	}
+}
