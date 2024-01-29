686390fe84f75559fc1495f057aa670094533154
devkgm <gm.kim333@gmail.com>
2024-01-29 11:20:25 +0900

    2

--- src/test/java/com/devkgm/app/DBTest.java
+++ src/test/java/com/devkgm/app/DBTest.java
@@ -11,24 +11,9 @@ public class DBTest extends MyTest {
     private SqlSession sqlSession;
     private String namespace = "com.devkgm.app.board.product.ProductDAO.";
 
-    //@Test
-    public void test() throws Exception {
-
-        for (int i = 1; i <= 100; i++) {
-            Double rate = Math.random() * 5;
-            Long sale = (long) Math.random() * 2;
-
-            ProductDTO productDTO = new ProductDTO();
-            productDTO.setName("Test" + i);
-            productDTO.setContent("Test Content" + i);
-            productDTO.setRate(rate);
-            productDTO.setIs_sale(sale);
-            ;
-            sqlSession.insert(namespace + "add", productDTO);
-
-            if (i % 10 == 0)
-                Thread.sleep(500);
-        }
+    @Test
+    void test() throws Exception {
+        
     }
 
 }
