3698c6490102527fca0bf09f0f91121b5ec1b43f
devkgm <gm.kim333@gmail.com>
2024-01-29 11:13:27 +0900

    FIX TEST

--- src/test/java/com/devkgm/app/DBTest.java
+++ src/test/java/com/devkgm/app/DBTest.java
@@ -7,28 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.devkgm.app.board.product.ProductDTO;
 
 public class DBTest extends MyTest {
-	@Autowired
-	private SqlSession sqlSession;
-	private String namespace = "com.devkgm.app.board.product.ProductDAO.";
+    @Autowired
+    private SqlSession sqlSession;
+    private String namespace = "com.devkgm.app.board.product.ProductDAO.";
 
-	@Test
-	public void test() throws Exception {
+    //@Test
+    public void test() throws Exception {
 
-		for (int i = 1; i <= 100; i++) {
-			Double rate = Math.random() * 5;
-			Long sale = (long) Math.random() * 2;
+        for (int i = 1; i <= 100; i++) {
+            Double rate = Math.random() * 5;
+            Long sale = (long) Math.random() * 2;
 
-			ProductDTO productDTO = new ProductDTO();
-			productDTO.setName("Test" + i);
-			productDTO.setContent("Test Content" + i);
-			productDTO.setRate(rate);
-			productDTO.setIs_sale(sale);
-			;
-			sqlSession.insert(namespace + "add", productDTO);
+            ProductDTO productDTO = new ProductDTO();
+            productDTO.setName("Test" + i);
+            productDTO.setContent("Test Content" + i);
+            productDTO.setRate(rate);
+            productDTO.setIs_sale(sale);
+            ;
+            sqlSession.insert(namespace + "add", productDTO);
 
-			if (i % 10 == 0)
-				Thread.sleep(500);
-		}
-	}
+            if (i % 10 == 0)
+                Thread.sleep(500);
+        }
+    }
 
 }
